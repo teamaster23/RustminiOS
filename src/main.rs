5853d956d00e418fbce31e82b9e801207c2c7801
MeteoricArC <mengcevip@126.com>
2024-06-03 22:02:39 +0800

    chapter3

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,13 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start()->!{
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-    
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop{};
 } 
 
 #[panic_handler]
 fn panic(_info:&PanicInfo)->!{
+    println!("{}", _info);
     loop{};
 }

0d09020fbb9d7136a59339d835447687c1a04ebe
MeteoricArC <mengcevip@126.com>
2024-06-03 21:12:27 +0800

    2024.6.3

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
 #![no_std]
 #![no_main]
-#![feature(asm)]
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start()->!{
-    let vga_buffer= 0xb8000 as *mut u8;
-
-    for(i,&byte) in HELLO.iter().enumerate() {
-        unsafe{
-            *vga_buffer.offset(i as isize*2)=byte;
-            *vga_buffer.offset(i as isize*2+1)=0xb;
-        }
-    }
-    
+    vga_buffer::print_something();
     loop{};
 } 
 

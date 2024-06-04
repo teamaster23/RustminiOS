43068d48d241b1cfc1e3d23f69f32f07c6e2a4d9
MeteoricArC <mengcevip@126.com>
2024-06-04 14:46:46 +0800

    fix_some_word_error

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -19,7 +19,7 @@ fn should_fail() {
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    serial_println!("[ok]");
+    serial_println!("[panicked]");
     exit_qemu(QemuExitCode::Success);
     loop {}
 }

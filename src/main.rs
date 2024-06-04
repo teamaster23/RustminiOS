5dfa98a28dfd1d0d11c999eed55f31c62389d458
MeteoricArC <mengcevip@126.com>
2024-06-04 10:07:50 +0800

    6.4

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
 #[no_mangle]
 pub extern "C" fn _start()->!{
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    
+    #[cfg(test)]
+    test_main();
+
     loop{};
 } 
 
@@ -15,4 +22,37 @@ pub extern "C" fn _start()->!{
 fn panic(_info:&PanicInfo)->!{
     println!("{}", _info);
     loop{};
+}
+
+#[cfg(test)]
+fn test_runner(tests:&[&dyn Fn()]){
+    serial_println!("Running {} tests", tests.len());
+    for test in tests{
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }

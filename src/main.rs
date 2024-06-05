87374a2c983c922322419fc945221efceed50015
Aer7z <570991904@qq.com>
2024-06-05 14:46:25 +0800

    第二章结束

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,25 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // 不重整函数名
-pub extern "C" fn _start() -> ! {
-    // 因为编译器会寻找一个名为`_start`的函数，所以这个函数就是入口点
-    // 默认命名为`_start`
-    loop {}
-}
-
 /// 这个函数将在panic时被调用
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // 禁用函数名重整
+pub extern "C" fn _start() -> ! {
+    // 获取VGA缓冲区的指针
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    // 无限循环
     loop {}
 }
